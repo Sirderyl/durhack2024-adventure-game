51fc69f8fef86c60b8f8b6605c35b29aedafe25f
Kieran <kieranknowles11@hotmail.co.uk>
2024-11-03 02:53:02 +0000

    Fix useEffect loop

--- App/web-ui/src/App.jsx
+++ App/web-ui/src/App.jsx
@@ -5,16 +5,17 @@ import Quest1 from './pages/Quest1'
 import './App.css'
 
 function App() {
+  const [story, setStory] = useState('self_worth')
 
   const [title, setTitle] = useState("")
   const [introduction, setIntroduction] = useState("")
   const [character_description,setCharacter_description] = useState("")
   const [location, setLocation] = useState("")
   const [quests, setQuests] = useState({})
-  
+
 
   useEffect(() => {
-    fetch("http://localhost:5000/story/self_worth")
+    fetch(`http://localhost:5000/story/${story}`)
     .then(response => response.json())
     .then(data => {
       setTitle(data.title)
@@ -24,14 +25,14 @@ function App() {
       setQuests(data.quests)
     })
     .catch(err => console.log(err))
-  }, [quests])
-  
+  }, [story])
+
 
   return (
     <div className='App'>
       <h1>{title}</h1>
       <Routes>
-        <Route path='/the-broken-crown' element={<Home introduction={introduction} 
+        <Route path='/the-broken-crown' element={<Home introduction={introduction}
         character_description={character_description} />} />
         <Route path='/the-broken-crown/quest1' element={<Quest1 location={location} quest={quests.journey_begins?.description} />} />
       </Routes>
